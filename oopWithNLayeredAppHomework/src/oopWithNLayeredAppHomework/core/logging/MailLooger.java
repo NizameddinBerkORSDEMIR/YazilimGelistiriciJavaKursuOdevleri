791a72c34d7f122935b57985aed470ed9e01fe05
NizameddinBerkORSDEMIR <114021421+NizameddinBerkORSDEMIR@users.noreply.github.com>
2022-10-14 10:33:16 +0300

    3.HaftaÖdevleri

---
+++ oopWithNLayeredAppHomework/src/oopWithNLayeredAppHomework/core/logging/MailLooger.java
@@ -0,0 +1,12 @@
+package oopWithNLayeredAppHomework.core.logging;
+
+public class MailLooger implements Logger{
+
+	@Override
+	public void log(String data) {
+		System.out.println("Maille Loglandı: "+ data);
+		
+	}
+
+
+}
